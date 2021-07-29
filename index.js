9631416b7cf9b1e952a02a2106237d5ebb4d50e1
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2021-07-29 17:04:26 +0300

    combine arrays

--- index.js
+++ index.js
@@ -35,3 +35,54 @@ console.log(abcde);
 const slice = abc.slice(1, 3);
 console.log(slice);
 
+//easy way to combine arrays;
+const combined = [...abc, 'sing', ...cde];
+console.log(combined);
+
+const copied = [...combined];
+
+// for of;
+for (number of numbers){
+    console.log(number);
+};
+
+//forEach()
+numbers.forEach((number, index) => console.log(index, number));
+
+//join()
+
+let num = [1, 8, 9, 0];
+let joins = num.join(',');
+console.log(joins);
+
+//split();
+
+let message = 'My name is Sarah';
+let splits = message.split(' ');
+console.log(splits);
+//reverse();
+let reverse = num.reverse();
+console.log(reverse);
+
+
+//sort();
+
+let sort = num.sort();
+console.log(sort);
+
+// sort() in object;
+
+const course = [
+    { id: 1, name: 'Node.js'},
+    { id: 2, name: 'JavaScript'}
+];
+
+course.sort(function(a, b) {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+
+    if(nameA < nameB) return -1;
+    if(nameA > nameB) return 1;
+    return 0
+});
+console.log(course);
