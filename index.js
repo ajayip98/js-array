10df088f485df6a1dce9d3ad2ef54f119c999d5e
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2021-08-02 15:40:29 +0300

    get max reduce()

--- index.js
+++ index.js
@@ -136,13 +136,17 @@
 //Ex. Getmax
 
 const numbers = [1, 2, 3, 4];
-const max = getMax([1, 2, 3]);
+const max = getMax([1, 2, 3, 8]);
 console.log(max);
 
+// function getMax(array) {
+//   if (array.length === 0) return undefined;
+//   let max = array[0];
+
+//   for (let i = 1; i < array.length; i++) if (array[i] > max) max = array[i];
+//   return max;
+// }
 function getMax(array) {
   if (array.length === 0) return undefined;
-  let max = array[0];
-
-  for (let i = 1; i < array.length; i++) if (array[i] > max) max = array[i];
-  return max;
+  return array.reduce((a, b) => (a > b ? a : b));
 }
