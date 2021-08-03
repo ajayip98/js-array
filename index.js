ccb1d73a7499c8e2c37747cc93b141ea0141bb2a
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2021-08-03 14:04:29 +0300

    Exercise Movies

--- index.js
+++ index.js
@@ -135,9 +135,9 @@
 
 //Ex. Getmax
 
-const numbers = [1, 2, 3, 4];
-const max = getMax([1, 2, 3, 8]);
-console.log(max);
+// const numbers = [1, 2, 3, 4];
+// const max = getMax([1, 2, 3, 8]);
+// console.log(max);
 
 // function getMax(array) {
 //   if (array.length === 0) return undefined;
@@ -146,7 +146,23 @@ console.log(max);
 //   for (let i = 1; i < array.length; i++) if (array[i] > max) max = array[i];
 //   return max;
 // }
-function getMax(array) {
-  if (array.length === 0) return undefined;
-  return array.reduce((a, b) => (a > b ? a : b));
-}
+// function getMax(array) {
+//   if (array.length === 0) return undefined;
+//   return array.reduce((a, b) => (a > b ? a : b));
+// }
+
+//Wx. movies;
+const movies = [
+  { title: "a", year: 2018, rating: 4.5 },
+  { title: "b", year: 2018, rating: 4.7 },
+  { title: "c", year: 2018, rating: 3 },
+  { title: "d", year: 2017, rating: 4.5 },
+];
+
+const titles = movies
+  .filter((m) => m.year === 2018 && m.rating >= 4)
+  .sort((a, b) => a.rating - b.rating)
+  .reverse()
+  .map((m) => m.title);
+
+console.log(titles);
