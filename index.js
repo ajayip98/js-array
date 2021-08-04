e3201a7834f8c2686d03e97e487995ac01231540
ajayip98 <ajayipnurberdiyeva98@gmail.com>
2021-08-04 17:35:49 +0300

    Ex.error handling

--- index.js
+++ index.js
@@ -194,3 +194,22 @@ const circle = {
 };
 
 console.log(circle.area);
+
+//Ex. Error handling
+try {
+  const numbers = [1, 2, 3, 4, 1];
+  const count = countOccurrence(null, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
+
+function countOccurrence(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error("Invalid array!");
+
+  return array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    console.log(accumulator, current, searchElement);
+    return accumulator + occurrence;
+  }, 0);
+}
